ddae7effc59cf53f069a380126e5755f559279c5
Robyn Tiger <n0908179@ntu.ac.uk>
2023-12-03 19:36:59 +0000

    Day 3 code

---
+++ C++ Solutions/day_3a.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+
+#include <iostream>
+#include <fstream>
+
+#include <string>
+#include <vector>
+
+bool checkIfNumber(char value){
+    if (isdigit(value)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int recursiveFindNum(std::string theString, int gridNum){
+    std::string s;
+
+    std::vector<int> vals;
+
+    for (int i = gridNum-1; i > gridNum-3; i--){
+        if (s.length() < 3){
+            if (i >= 0 && i < theString.length()){
+            if (checkIfNumber(theString[i])){
+                s = theString[i]+s;
+            }
+            else{
+                break;
+            }
+        }
+        }
+    }
+
+    s.push_back(theString[gridNum]);
+
+    for (int i = gridNum+1; i < gridNum+3; i++){
+        if (s.length() < 3){
+            if (i >= 0 && i < theString.length()){
+            if (checkIfNumber(theString[i])){
+                s.push_back(theString[i]);
+            }
+            else{
+                break;
+            }
+        }
+        }
+    }
+
+    return std::stoi(s);
+}
+
+int main(){
+    // Read file
+    std::ifstream MyReadFile("day_3.txt");
+    std::vector<std::string> grid;
+    
+    std::string line;
+
+    // all vals
+    std::vector<int> allVals;
+
+    // Go through each line in file
+    while (std::getline(MyReadFile, line)) {
+        grid.push_back(line);
+    }
+
+    for (int i =0; i < grid.size(); i++){
+        //std::cout << grid.at(i) << std::endl;
+
+        for (int j =0; j < grid.at(i).length(); j++){
+            //std::cout << grid.at(i)[j];
+            if (isdigit(grid.at(i)[j]) || grid.at(i)[j] == *"."){
+                continue;
+            }
+            else{
+                if (i > 0){
+                    if (j > 0){
+                        if (checkIfNumber(grid.at(i-1)[j-1])){
+                            allVals.push_back(recursiveFindNum(grid.at(i-1), j-1));
+                        }
+                    }
+                    if (j < line.length()){
+                        if (checkIfNumber(grid.at(i-1)[j+1])){
+                            if (allVals.back() != recursiveFindNum(grid.at(i-1), j+1)){
+                                allVals.push_back(recursiveFindNum(grid.at(i-1), j+1));
+                            }
+                        }
+                    }
+
+                    if (checkIfNumber(grid.at(i-1)[j])){
+                        if (allVals.back() != recursiveFindNum(grid.at(i-1), j)){
+                                allVals.push_back(recursiveFindNum(grid.at(i-1), j));
+                            }
+                        }
+                }
+
+                if (j > 0){
+                    if (checkIfNumber(grid.at(i)[j-1])){
+                        allVals.push_back(recursiveFindNum(grid.at(i), j-1));
+                        }
+                }
+
+                if (j < line.length()-1){
+                    if (checkIfNumber(grid.at(i)[j+1])){
+                        allVals.push_back(recursiveFindNum(grid.at(i), j+1));
+                        }
+                }
+
+                if (i < grid.size()){
+                    if (j > 0){
+                        if (checkIfNumber(grid.at(i+1)[j-1])){
+                            allVals.push_back(recursiveFindNum(grid.at(i+1), j-1));
+                        }
+                    }
+                    if (j < line.length()){
+                        if (checkIfNumber(grid.at(i+1)[j+1])){
+                            if (allVals.back() != recursiveFindNum(grid.at(i+1), j+1)){
+                            allVals.push_back(recursiveFindNum(grid.at(i+1), j+1));
+                        }
+                        }
+                    }
+
+                    if (checkIfNumber(grid.at(i+1)[j])){
+                        if (allVals.back() != recursiveFindNum(grid.at(i+1), j)){
+                            allVals.push_back(recursiveFindNum(grid.at(i+1), j));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    int sum = 0;
+
+    for (int i =0; i < allVals.size(); i++){
+        sum += allVals.at(i);
+    }
+
+    std::cout << sum;
+
+    // Close file
+    MyReadFile.close();
+}
